e6ff005f716db1ec1803a19a84cbe3a387100994
zaynabzya <137387906+zaynabzya@users.noreply.github.com>
2024-06-23 22:51:25 +0000

    Pending changes exported from your codespace

--- pencilsketch.py
+++ pencilsketch.py
@@ -1,40 +1,29 @@
-
-import streamlit as st
-import numpy as np
-
 import cv2
 from io import BytesIO
-import matplotlib.pyplot as plt
+import streamlit as st
+import numpy as np
 
 st.title("PencilPal")
 
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png"])
 
+Image = None  # Initialize Image variable
+
 if uploaded_image:
     # Read image using OpenCV
-    Image = cv2.imdecode(np.frombuffer(uploaded_image.read(), np.uint8), cv2.IMREAD_COLOR)
+    image_data = uploaded_image.read()
+    nparr = np.frombuffer(image_data, np.uint8)
+    Image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    Image = cv2.cvtColor(Image, cv2.COLOR_BGR2RGB)  # Convert to RGB for display
 
-Image = cv2.cvtColor(Image, cv2.COLOR_BGR2RGB)
+if Image is not None:
+    inverted_image = 255 - Image
+    pencil_sketch = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+    st.image(pencil_sketch, caption="Pencil Sketch")
 
-plt.imshow(Image)
-plt.title("Original Image")
-plt.axis('off')  # Hide the axis
-plt.show()
-
-inverted_image = 255 - Image
-plt.imshow(inverted_image)
-plt.title("Inverted Image")
-plt.axis('off')  # Hide the axis
-plt.show()
-
-pencil_sketch = cv2.GaussianBlur(inverted_image, (21, 21), 0)
-plt.imshow(blurred_image)
-plt.title("Blurred Image")
-plt.axis('off')  # Hide the axis
-plt.show()
-
-st.image(pencil_sketch, caption="Pencil Sketch")
-
-buf = BytesIO()
-cv2.imencode('.jpg', pencil_sketch, [cv2.IMWRITE_JPEG_QUALITY, 80])[1].tobytes()
-st.download_button("Download Pencil Sketch", buf.getvalue(), "pencil_sketch.jpg", "image/jpg")
+    # Prepare download button
+    buf = BytesIO()
+    cv2.imencode('.jpg', pencil_sketch, [cv2.IMWRITE_JPEG_QUALITY, 80])[1].tofile(buf)
+    st.download_button("Download Pencil Sketch", buf.getvalue(), "pencil_sketch.jpg", "image/jpg")
+else:
+    st.write("Upload an image to generate a pencil sketch.")
